a8112a2184c74afd39a133a779c769b0c6dcc7ac
RBRNFRVR <rommelchavez99@gmail.com>
2020-03-24 21:43:29 -0400

    JosephChavez1

--- db/migrate/20200323192217_create_exchanges.rb
+++ db/migrate/20200323192217_create_exchanges.rb
@@ -1,10 +1,7 @@
 class CreateExchanges < ActiveRecord::Migration[6.0]
   def change
     create_table :exchanges do |t|
-      t.integer :restaurant_id
-      t.integer :organization_id
-      t.string :exchange_hours
-      t.string :notes
+    
 
       t.timestamps
     end
