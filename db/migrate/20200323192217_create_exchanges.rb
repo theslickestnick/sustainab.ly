f1b10c33cbb0b2473c74da4abf92ac4ee145475c
RBRNFRVR <rommelchavez99@gmail.com>
2020-03-24 21:50:36 -0400

    lost

--- db/migrate/20200323192217_create_exchanges.rb
+++ db/migrate/20200323192217_create_exchanges.rb
@@ -1,7 +1,12 @@
 class CreateExchanges < ActiveRecord::Migration[6.0]
   def change
     create_table :exchanges do |t|
-    
+      t.integer :restaurant_id
+      t.integer :organization_id
+      t.datetime :exchange_start
+      t.datetime :exchange_end
+      t.boolean :status
+      t.string :notes
 
       t.timestamps
     end
