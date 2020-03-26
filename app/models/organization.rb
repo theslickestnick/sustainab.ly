06506ace4b6b8f43bb06ffc1b499bcc5359c7e6c
RBRNFRVR <rommelchavez99@gmail.com>
2020-03-25 21:15:01 -0400

    morework

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,9 @@
 class Organization < ApplicationRecord
     has_many :exchanges
     has_many :restaurants, through: :exchanges
+
+# validates :phone_number, length: {is: 10}
+# validates :name, :street_address, :city, :state, :zip_code, :contact_email, :phone_number, presence: true
+# validates :zip_code, length: {is: 5}
+# validates :contact_email, uniqueness: true
 end
