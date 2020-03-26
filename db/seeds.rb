c770aa58329b0d77a0b7cda3561f159d8d037265
RBRNFRVR <rommelchavez99@gmail.com>
2020-03-26 15:19:36 -0400

    lotswork

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,29 +9,23 @@
 Restaurant.destroy_all
 RestaurantFood.destroy_all
 Exchange.destroy_all
-Organzation.destory_all
-
+Organization.destroy_all
 20.times do 
-    Restaurant.create(:name => Faker::Restaurant.name, :street_address => Faker::Address.street_address, 
+    Restaurant.create!(:name => Faker::Restaurant.name, :street_address => Faker::Address.street_address, 
     :city => Faker::Address.city, :state => Faker::Address.state, :zip_code => Faker::Address.zip_code, 
-    :contact_email => Faker::Internet.email, :phone_number => Faker::PhoneNumber.phone_number)
+    :contact_email => Faker::Internet.email, :phone_number => Faker::PhoneNumber.phone_number, :password_digest => Faker::Number.number(digits: 5))
 end
-
-150.times do 
-    RestaurantFood.create(:food_type => Faker::Food.dish, :quantity => rand(20..100), :restaurant_id => Restaurant.all.sample.id, :use_by => Faker::Date.forward(days: 100), :notes => Faker::Lorem.paragraph)
-<<<<<<< HEAD
+50.times do 
+    RestaurantFood.create!(:food_type => Faker::Food.dish, :quantity => rand(20..50), :restaurant_id => Restaurant.all.sample.id, :use_by => Faker::Date.forward(days: 100), :notes => Faker::Lorem.paragraph)
 end
-
 30.times do
     Organization.create(:name => Faker::Restaurant.name, :street_address => Faker::Address.street_address,
         :city => Faker::Address.city, :state => Faker::Address.state, :zip_code => Faker::Address.zip_code,
         :contact_email => Faker::Internet.email, :phone_number => Faker::PhoneNumber.phone_number)
 end
-
 5.times do
     Exchange.create(restaurant_id: Restaurant.all.sample.id , organization_id: Organization.all.sample.id, status: false, notes: Faker::Lorem.paragraph)
 end
-
 10.times do
     Exchange.create(restaurant_id: Restaurant.all.sample.id , status: true, notes: Faker::Lorem.paragraph)
 end
